d53534183ae7b6eae4230455f000a687868f872d
Sangeeth_ks <sangeeth.ks@mobiotics.com>
2018-02-15 12:17:31 +0530

    Initial commit

---
+++ platforms/ios/build/nativescripthandson.build/Debug-iphonesimulator/nativescripthandson.build/Objects-normal/i386/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/sangeethks/Projects/workouts/nativescript-hands-on/platforms/ios/nativescripthandson/nativescripthandson-Prefix.pch \
+  /Users/sangeethks/Projects/workouts/nativescript-hands-on/platforms/ios/internal/main.m \
+  /Users/sangeethks/Projects/workouts/nativescript-hands-on/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/sangeethks/Projects/workouts/nativescript-hands-on/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/sangeethks/Projects/workouts/nativescript-hands-on/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/sangeethks/Projects/workouts/nativescript-hands-on/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/sangeethks/Projects/workouts/nativescript-hands-on/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/sangeethks/Projects/workouts/nativescript-hands-on/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/sangeethks/Projects/workouts/nativescript-hands-on/platforms/ios/internal/TNSDebugging.h \
+  /Users/sangeethks/Projects/workouts/nativescript-hands-on/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
